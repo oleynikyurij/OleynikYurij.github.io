81e76e7f536e075683bd4fd035a671489d914232
Yurij Oleynik <2010nwa@gmail.com>
2019-01-20 16:49:39 +0200

    Add files via upload

---
+++ www8/js/script.js
@@ -0,0 +1,56 @@
+let mobileMenu = document.querySelector('.menu-toggle');
+let overlay = document.querySelector('.overlay');
+let mobile = document.querySelector('.mobile');
+let menuToggle = document.querySelector('.menu-toggle');
+
+let btnDrop = document.querySelector('.menu-item-link__active');
+let dropDown = document.querySelector('.drop-down');
+
+let input = document.querySelector('#text');
+
+mobileMenu.addEventListener('click', (e) => {
+	e.preventDefault();
+	overlay.classList.toggle('dn');
+	mobileMenu.classList.toggle('dn');
+	mobile.classList.toggle('dn');
+	menuToggle.classList.toggle('toggled-on');
+});
+
+btnDrop.addEventListener('click', (e) => {
+	e.preventDefault();
+	dropDown.classList.toggle('dn');
+});
+
+dropDown.addEventListener('click', (e) => {
+	e.preventDefault();
+	dropDown.classList.toggle('dn');
+});
+
+let qestionsTitle = document.querySelectorAll('.qestions-title');
+let qestionsContent = document.querySelectorAll('.qestions-content');
+let qestionsImg = document.querySelectorAll('.qestions-img');
+
+
+qestionsTitle.forEach((item, i) => {
+
+	item.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		if (e.currentTarget == item && qestionsContent[i].classList.contains('content-active')) {
+
+			qestionsContent[i].classList.remove('content-active');
+			qestionsImg[i].classList.remove('rotate');
+		} else {
+			qestionsContent.forEach((item) => {
+				item.classList.remove('content-active');
+			});
+			qestionsImg.forEach((item) => {
+				item.classList.remove('rotate');
+			});
+			qestionsContent[i].classList.add('content-active');
+			qestionsImg[i].classList.add('rotate');
+		}
+
+	})
+
+});
