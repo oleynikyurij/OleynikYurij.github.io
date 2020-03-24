a67511f1f0873b9a3a35a0b2d58cedaadfa445c0
Yurij Oleynik <2010nwa@gmail.com>
2020-03-24 15:39:47 +0200

    conguru
    conguru

---
+++ conguru/precache-manifest.d80d1fb9c6098e20792b955e422f24dc.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "77eae99c56e50eacfb89",
+    "url": "/conguru/css/app.2e815844.css"
+  },
+  {
+    "revision": "77eae99c56e50eacfb89",
+    "url": "/conguru/js/app.3d413c62.js"
+  },
+  {
+    "revision": "8cf502b7c0769366fe15",
+    "url": "/conguru/js/chunk-vendors.90f9807d.js"
+  },
+  {
+    "revision": "d63e2069f1af6bc34810ffdf479e27f8",
+    "url": "/conguru/img/event-4.d63e2069.jpg"
+  },
+  {
+    "revision": "eae17d3e0b66aceefa5abc8a719aa3ca",
+    "url": "/conguru/img/3.eae17d3e.jpg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/conguru/img/linkedin.4b475fb3.svg"
+  },
+  {
+    "revision": "8ce6711a393e46a33462da8df20f37d5",
+    "url": "/conguru/img/like.8ce6711a.svg"
+  },
+  {
+    "revision": "893d2b57ca13e5da99d1495328e40f14",
+    "url": "/conguru/img/marker.893d2b57.svg"
+  },
+  {
+    "revision": "c0cd138271276bb33a87c2d174015455",
+    "url": "/conguru/img/facebook.c0cd1382.svg"
+  },
+  {
+    "revision": "91da33316a9345e053c8b6981d16c405",
+    "url": "/conguru/img/no-ok.91da3331.svg"
+  },
+  {
+    "revision": "a3c07b53fe6b7ec75da931ec18b04285",
+    "url": "/conguru/img/search.a3c07b53.svg"
+  },
+  {
+    "revision": "d63e2069f1af6bc34810ffdf479e27f8",
+    "url": "/conguru/img/event-1.d63e2069.jpg"
+  },
+  {
+    "revision": "ffd458f9e4b87c0d23cd02cc90c1f2f5",
+    "url": "/conguru/img/ok.ffd458f9.svg"
+  },
+  {
+    "revision": "9bf9e89b33ad3d85212ee79df9baebc7",
+    "url": "/conguru/img/people.9bf9e89b.svg"
+  },
+  {
+    "revision": "5eec5200c41d92a2409979d78cec50b1",
+    "url": "/conguru/img/share.5eec5200.svg"
+  },
+  {
+    "revision": "09aea0f59807f6f4f66af7f5719cba9e",
+    "url": "/conguru/img/google.09aea0f5.svg"
+  },
+  {
+    "revision": "60017fbecb146dcf437ff762ce75cd0f",
+    "url": "/conguru/img/event-2.60017fbe.jpg"
+  },
+  {
+    "revision": "26f14547fb0cfdc20814f13804523d69",
+    "url": "/conguru/index.html"
+  },
+  {
+    "revision": "eae17d3e0b66aceefa5abc8a719aa3ca",
+    "url": "/conguru/image/3.jpg"
+  },
+  {
+    "revision": "d63e2069f1af6bc34810ffdf479e27f8",
+    "url": "/conguru/image/event-1.jpg"
+  },
+  {
+    "revision": "60017fbecb146dcf437ff762ce75cd0f",
+    "url": "/conguru/image/event-2.jpg"
+  },
+  {
+    "revision": "d63e2069f1af6bc34810ffdf479e27f8",
+    "url": "/conguru/image/event-4.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/conguru/robots.txt"
+  }
+];
